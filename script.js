5f3e95368f6c8b96ddda116a6a7add7d903e370b
Reetesh kumar <115656541+Reetesh0007@users.noreply.github.com>
2024-06-18 08:05:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: [ "Developer", "Designer", "Freelancer","YouTuber","vlogger"],
+        strings: [ "Web Developer", "Designer","YouTuber","vlogger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
